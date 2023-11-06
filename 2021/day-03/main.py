d85d6a1c9d741c6430d9810be5c5dcce064bbe5f
Adrián García Vázquez <adriangvazquez@gmail.com>
2023-11-06 16:02:32 +0100

    Remove input.txt and test.txt

---
+++ 2021/day-03/main.py
@@ -0,0 +1,15 @@
+from aocd import get_data
+
+data = get_data(day=3, year=2021).split('\n')
+
+
+def part_one():
+    return data
+
+
+def part_two():
+    return
+
+
+print(part_one())
+print(part_two())
