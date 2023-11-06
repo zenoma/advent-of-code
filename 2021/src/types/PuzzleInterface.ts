f1d488215a9b57a3fd7972c2664c47bdd48125b4
Adrián García Vázquez <adriangvazquez@gmail.com>
2023-11-06 14:50:16 +0100

    Delete files from AoC 2021

--- 2021/src/types/PuzzleInterface.ts
+++
@@ -1,8 +0,0 @@
-export interface PuzzleInterface {
-  solveFirst: () => string;
-  solveSecond: () => string;
-  getFirstExpectedResult: () => string;
-  getSecondExpectedResult: () => string;
-  partOne: () => string;
-  partTwo: () => string;
-}
--- 2021/src/utils/readFile.ts
+++
@@ -1,5 +0,0 @@
-import * as fs from 'fs';
-
-export default (inputFilePath: string) => {
-	return fs.readFileSync(inputFilePath, 'utf8');
-};
